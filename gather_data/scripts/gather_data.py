c2eb5ed715c215a4b5f423ee6eb088d481ba1dee
david-alejo <churr.te@gmail.com>
2018-07-26 13:20:52 +0200

    Minor changes

--- gather_data/scripts/gather_data.py
+++ gather_data/scripts/gather_data.py
@@ -2,14 +2,17 @@
 # license removed for brevity
 import rospy
 from tf import TransformListener
+import tf
 from rssi_get.msg import Nvip_status
 from libelium_waspmote_gas_node.msg import GasMeasure
+#from tf import TransformError
+#from tf import TfError
 
 def callback(data):
   #if tf_listener.frameExists("/base_link") and tf_listener.frameExists("/odom"):
   try:
     t = tf_listener.getLatestCommonTime("/odom", "/base_link")
-    pos, ori = tf_listener.lookupTransform("/odom", "/base_link", t)
+    pos, ori = tf_listener.lookupTransform("/odom", "/base_link", rospy.Time(0))
     #if  tf_listener.frameExists("/map"):
       #t = tf_listener.getLatestCommonTime("/map", "/base_link")
       #pos_2, ori_2 = tf_listener.lookupTransform("/map", "/base_link",  t)
@@ -17,18 +20,20 @@ def callback(data):
     #else:
       #text_file.write("%f %f 0.0 0.0 %d\n"%(pos[0], pos[1], data.rssi))
     #print pos
-    text_file.write("%f %f %d\n"%(pos[0], pos[1], data.rssi))
-    print("%f %d\n"%( pos[0], data.rssi))
-  except TransformError:
+    
+    text_file.write("%f %f %f %d\n"%(t.secs + t.nsecs * 1e-9, pos[0], pos[1], data.rssi))
+
+    print("%f %f %f %d\n"%( t.secs + t.nsecs * 1e-9, pos[0], pos[1], data.rssi))
+  except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
     print("rssi callback: tf trouble")
     
 def gas_callback(data):
   try:
     t = tf_listener.getLatestCommonTime("/odom", "/base_link")
-    pos, ori = tf_listener.lookupTransform("/odom", "/base_link", t)
-    gas_text_file.write("%f %f %f %f %f %f %f %f %f\n"%(pos[0], pos[1], data.O2_conc, data.H2S_conc, data.CO_conc, data.CH4_conc, data.temperature, data.pressure, data.RH))
-    print("Gas measure: %f %f %f %f %f %f %f %f %f\n"%(pos[0], pos[1], data.O2_conc, data.H2S_conc, data.CO_conc, data.CH4_conc, data.temperature, data.pressure, data.RH))
-  except TfError:
+    pos, ori = tf_listener.lookupTransform("/odom", "/base_link", rospy.Time(0))
+    gas_text_file.write("%f %f %f %f %f %f %f %f %f %f\n"%(t.secs + t.nsecs * 1e-9, pos[0], pos[1], data.O2_conc, data.H2S_conc, data.CO_conc, data.CH4_conc, data.temperature, data.pressure, data.RH))
+    print("Gas measure: %f %f %f %f %f %f %f %f %f %f\n"%(t.secs + t.nsecs * 1e-9, pos[0], pos[1], data.O2_conc, data.H2S_conc, data.CO_conc, data.CH4_conc, data.temperature, data.pressure, data.RH))
+  except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
     print("gas_callback: tf trouble")
     
 if __name__ == '__main__':
@@ -38,7 +43,7 @@ if __name__ == '__main__':
   rospy.Subscriber("/gas_info", GasMeasure, gas_callback)
   text_file = open('rssi.txt', 'w')
   gas_text_file = open('gas.txt', 'w')
-  gas_text_file.write("% x y o2 h2s co ch4 temp pressure RH\n");
+  gas_text_file.write("% t x y o2 h2s co ch4 temp pressure RH\n");
   rospy.spin()
   text_file.close()
   
