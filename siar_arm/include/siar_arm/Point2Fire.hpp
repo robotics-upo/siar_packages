a3b5d6e750f06b8277d08bfaa731072bb491fd62
gonmiermu <gonzalo_skater94@hotmail.com>
2019-11-11 12:41:38 +0100

    Added limits to the motor commands

--- siar_arm/include/siar_arm/Point2Fire.hpp
+++ siar_arm/include/siar_arm/Point2Fire.hpp
@@ -52,30 +52,42 @@ class Point2Fire
 		ros::Publisher arm_pan_pub_;
 		ros::Publisher arm_tilt_pub_;
 		
-		std::string robot_name_;
+		std::string mot_pan_arm_file_{};
+		std::string mot_tilt_arm_file_{};
+		std::string robot_name_{};
 		
 		ros::Time   last_command_time_ { ros::Time::now() };
 		
 		Pid pan_control_;
 		Pid tilt_control_;
 		
+		
+		
 		float pan_ref_{0.0};
 		float tilt_ref_{0.0};
+		float pan_center_{0.0};
+		float tilt_center_{0.0};		
+		float pan_min_{0.0};
+		float tilt_min_{0.0};
+		float pan_max_{0.0};
+		float tilt_max_{0.0};
 		
 		float fire_offset_x_, fire_offset_y_;
 		
 		std::experimental::optional<Point2D<float>> center_img_;
 		std::experimental::optional<Point2D<float>> last_fire_detected_;
 		float area_fire_detected_{0.0};
-		
-		
+				
 		Point2Fire(ros::NodeHandle &nh, ros::NodeHandle &pnh);
 		~Point2Fire() = default;
+		
+		void load_data(const std::string &mot_pan_arm_file, const std::string &mot_tilt_arm_file) ;
+		
 		
 		void FireDetectCallback(const fireawareness_ros::FireDetections2D::ConstPtr& msg);
 		void CamInfoCallback(const sensor_msgs::CameraInfo::ConstPtr& msg);
 		void ControlLoop( void);
-		std_msgs::Float32 ComputePID( Pid& _pid, float& _ref, float _p,  float _offset, const ros::Duration& _t );	
+		std_msgs::Float32 ComputePID( Pid& _pid, float& _ref, float _p,  float _offset, const ros::Duration& _t, float _limit_min, float _limit_max  );	
 };		
 }
 
--- siar_arm/src/Point2Fire.cpp
+++ siar_arm/src/Point2Fire.cpp
@@ -8,6 +8,9 @@ namespace SIAR
 		float kp_tilt,ki_tilt,kd_tilt;
 		float kp_pan,ki_pan,kd_pan;
 		
+		pnh.param<std::string>("mot_pan_arm_file", mot_pan_arm_file_, "");
+		pnh.param<std::string>("mot_tilt_arm_file", mot_tilt_arm_file_, "");
+		
 		pnh.param<std::string>("robot_name", robot_name_,"siar");
 		pnh.param<float>("kp_pan", kp_pan,1.0);
 		pnh.param<float>("ki_pan", ki_pan,0.0);
@@ -20,6 +23,9 @@ namespace SIAR
 		
 		pan_control_ = control_toolbox::Pid(kp_pan,ki_pan,kd_pan);
 		tilt_control_ = control_toolbox::Pid(kp_tilt,ki_tilt,kd_tilt);
+		
+		load_data(mot_pan_arm_file_, mot_tilt_arm_file_) ;
+		
 							
 		fire_detec_sub_ = nh.subscribe( "/firedetections2D", 2,  &Point2Fire::FireDetectCallback, this);
 		fire_cam_info_sub_ = nh.subscribe("/" + robot_name_ + "/thermal_camera/camera_info", 2,  &Point2Fire::CamInfoCallback, this);
@@ -27,6 +33,27 @@ namespace SIAR
 		arm_pan_pub_ = nh.advertise<std_msgs::Float32>("/" + robot_name_ +"/arm_pan", 2);
 		arm_tilt_pub_ = nh.advertise<std_msgs::Float32>("/" + robot_name_ +"/arm_tilt", 2);					
 	}	
+
+	void Point2Fire::load_data(const std::string &mot_pan_arm_file, const std::string &mot_tilt_arm_file) 
+	{
+		std::ifstream infile_pan(mot_pan_arm_file_);
+		infile_pan >> pan_min_;
+		while( infile_pan >> pan_max_ ){};
+		if( pan_min_ > pan_max_ )
+		{
+			std::swap(pan_min_, pan_max_);				
+		} 
+		pan_ref_ = pan_center_ = (pan_min_ + pan_max_)/2.0;
+
+		std::ifstream infile_tilt(mot_tilt_arm_file_);
+		infile_tilt >> tilt_min_;
+		while( infile_tilt >> tilt_max_ ){};
+		if( tilt_min_ > tilt_max_ )
+		{
+			std::swap(tilt_min_, tilt_max_);				
+		} 
+		tilt_ref_ = tilt_center_ = (tilt_min_ + tilt_max_)/2.0;
+	}		
 
 	void Point2Fire::FireDetectCallback(const fireawareness_ros::FireDetections2D::ConstPtr& msg)
 	{
@@ -47,6 +74,8 @@ namespace SIAR
 		}
 		if( !fire_detected )
 		{
+			pan_ref_ = pan_center_;
+			tilt_ref_ = tilt_center_;
 			last_command_time_ = ros::Time::now();
 			last_fire_detected_ = {};
 		}
@@ -63,16 +92,19 @@ namespace SIAR
 		{			
 			const auto& current_time  = ros::Time::now();
 			
-			arm_pan_pub_.publish(ComputePID( pan_control_, pan_ref_ , last_fire_detected_->x,  fire_offset_x_, current_time - last_command_time_ ));
-			arm_tilt_pub_.publish(ComputePID( tilt_control_, tilt_ref_ , last_fire_detected_->y,  fire_offset_y_, current_time - last_command_time_ ));					
+			ROS_INFO("ARM PAN %f %f", pan_ref_ , last_fire_detected_->x);
+			ROS_INFO("ARM TILT %f %f", tilt_ref_ , last_fire_detected_->y);
+			arm_pan_pub_.publish(ComputePID( pan_control_, pan_ref_ , last_fire_detected_->x,  fire_offset_x_, current_time - last_command_time_ , pan_min_, pan_max_));
+			arm_tilt_pub_.publish(ComputePID( tilt_control_, tilt_ref_ , last_fire_detected_->y,  fire_offset_y_, current_time - last_command_time_, tilt_min_, tilt_max_ ));					
 			last_command_time_ = current_time;
 		}
 	}
 	
-	std_msgs::Float32 Point2Fire::ComputePID( Pid& _pid, float& _ref, float _p,  float _offset, const ros::Duration& _t )
+	std_msgs::Float32 Point2Fire::ComputePID( Pid& _pid, float& _ref, float _p,  float _offset, const ros::Duration& _t, float _limit_min, float _limit_max )
 	{
 		std_msgs::Float32 force;
 		 _ref += _pid.computeCommand( _p - _offset, _t);
+		 _ref = (((_ref>_limit_max)?_limit_max:_ref)<_limit_min)?_limit_min:_ref;
 		 force.data =_ref ;
 		return force;
 	}
