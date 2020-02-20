d3ee5d84ac15184627a3e283b75858e16758f17b
David Alejo <daletei@upo.es>
2020-02-20 06:09:36 +0100

    Version tested successfully

--- odom_to_tf/scripts/odom_to_tf.py
+++ odom_to_tf/scripts/odom_to_tf.py
@@ -2,12 +2,14 @@
 import roslib
 roslib.load_manifest('odom_to_tf')
 import rospy
+import numpy as np
 
 import tf
 import tf2_ros
 from nav_msgs.msg import Odometry
 import geometry_msgs.msg
 from sensor_msgs.msg import Imu
+import tf_conversions.posemath as pm
 
 class OdomToTF:
   def __init__(self):
@@ -18,32 +20,55 @@ class OdomToTF:
     self.override_stamp = rospy.get_param('~overridestamp', default=False)
     rospy.Subscriber("odom", Odometry, self.odometryCb)  
     rospy.Subscriber("imu", Imu, self.imuCb)
+    
+    self.br = tf2_ros.TransformBroadcaster()
+    
+    # Initial transforms
+    self.last_published_transform = pm.fromMatrix(np.eye(4))
+    self.last_odom_transform = pm.fromMatrix(np.eye(4))
+    
 
   def odometryCb(self, msg):
-    br = tf2_ros.TransformBroadcaster()
+    
     t = geometry_msgs.msg.TransformStamped()
-    if override_stamp:
+    if self.override_stamp:
       t.header.stamp = rospy.Time.now()
     else:
       t.header.stamp = msg.header.stamp
-    t.header.frame_id = odometry_frame
-    t.child_frame_id = base_link
-    t.transform.translation.x = msg.pose.pose.position.x
-    t.transform.translation.y = msg.pose.pose.position.y
-    t.transform.translation.z = msg.pose.pose.position.z
-    if self.last_imu_orientation == None:
-      t.transform.rotation.x = msg.pose.pose.orientation.x
-      t.transform.rotation.y = msg.pose.pose.orientation.y
-      t.transform.rotation.z = msg.pose.pose.orientation.z
-      t.transform.rotation.w = msg.pose.pose.orientation.w
-    else:
-      t.transform.rotation.x = self.last_imu_orientation.x
-      t.transform.rotation.x = self.last_imu_orientation.x
-      t.transform.rotation.x = self.last_imu_orientation.x
-      t.transform.rotation.x = self.last_imu_orientation.x
-      
-      
-    br.sendTransform(t)
+    t.header.frame_id = self.odometry_frame
+    t.child_frame_id = self.base_link
+    
+    position = (msg.pose.pose.position.x, msg.pose.pose.position.y, msg.pose.pose.position.z)
+    orientation = (msg.pose.pose.orientation.x, msg.pose.pose.orientation.y,
+                   msg.pose.pose.orientation.z, msg.pose.pose.orientation.w)
+    
+    # self.current_odom_transform = tf.TransformerROS.fromTranslationRotation(position, orientation)
+    self.current_odom_transform = pm.fromTf( (position, orientation))
+    delta_t = self.last_odom_transform.Inverse() * self.current_odom_transform
+    self.last_published_transform = self.last_published_transform * delta_t
+    
+    a = pm.toMsg(self.last_published_transform)
+    
+    
+    if self.last_imu_orientation != None: 
+      a.orientation = self.last_imu_orientation
+      self.last_odom_transform = pm.fromMsg(a)
+    t.transform.rotation.x = a.orientation.x
+    t.transform.rotation.y = a.orientation.y
+    t.transform.rotation.z = a.orientation.z
+    t.transform.rotation.w = a.orientation.w
+    
+    # print a
+    
+    t.transform.translation.x = self.last_published_transform[(0,3)]
+    t.transform.translation.y = self.last_published_transform[(1,3)]
+    t.transform.translation.z = self.last_published_transform[(2,3)]
+    
+    self.last_odom_transform = self.current_odom_transform
+    
+    
+    
+    self.br.sendTransform(t)
     
   def imuCb(self, msg):
     self.last_imu_orientation = msg.orientation
@@ -51,4 +76,5 @@ class OdomToTF:
 if __name__ == '__main__':
     last_quaternion = None
     rospy.init_node('odom_to_tf_broadcaster')
+    a = OdomToTF()
     rospy.spin()
